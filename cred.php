f6ad89ebd9247f1f83e253ed4e4f54995eeb478b
Muthukumaran <muthuishere@gmail.com>
2016-04-26 20:44:34 +0530

    adding php style

--- cred.php
+++ cred.php
@@ -2,5 +2,5 @@
 $_YOUR_APP_ID = '217849308606209';
 
 $_YOUR_APP_SECRET = 'dbff04bb1eedd39ca59815dea1ded653';
-$callback_url = 'https://secure-dusk-97128.herokuapp.com/';// example.com/login.php
+$callback_url = 'https://polar-hollows-39218.herokuapp.com/';// example.com/login.php
 ?>
